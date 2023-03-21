a8a523c37049361f114883492be70ee2543c5f3f
dynamic2code <dynamicdesingns@gmail.com>
2023-03-21 12:46:38 -0700

    changes to task 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -7,26 +7,26 @@
 
 void jack_bauer(void)
 {
-int a, b, hour, minute;
-for (hour = 0; hour < 24; hour++)
-{
-for (minute = 0; minute < 60; minute++)
-{
-if (hour % 10 && minute % 10)
-{
-_putchar('hour');
-_putchar(':');
-_putchar('minute');
-}
-else
-{
-a = 0;
-b = 0;
-_putchar('a');
-_putchar('hour');
-_putchar(':');
-_putchar('b');
-_putchar('minute');
-}
-}
+	int a, b, c, d;
+
+	for (a = 48; a <= 50; a++)
+	{
+		for (b = 48; b <= 57; b++)
+		{
+			for (c = 48; c <= 53; c++)
+			{
+				for (d = 48; d <= 57; d++)
+				{
+					if (a >= 50 && b >= 52)
+					break;
+					_putchar(a);
+					_putchar(b);
+					_putchar(58);
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+			}
+		}
+	}
 }
