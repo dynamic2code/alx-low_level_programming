8ad9f7dfd6d62641f7b692a65a8e3e2e9eb64f4f
dynamic2code <dynamicdesingns@gmail.com>
2023-03-21 13:39:21 -0700

    changes to task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,25 +6,25 @@
  */
 void print_to_98(int n)
 {
-	if (n >= 98)
-	{
-	for (; n > 98; n--)
-		{
-		_putchar(n);
-		_putchar(',');
-		}
-	}
-	else if (n < 98)
-	{
-		for (; n < 98; n++)
-		{
-		_putchar(n);
-		_putchar(',');
-		}
-	}
-	else
-	{
-	        _putchar(n);
-	}
-	_putchar('\n');
+if (n >= 98)
+{
+for (; n > 98; n--)
+{
+_putchar(n);
+_putchar(',');
+}
+}
+else if (n < 98)
+{
+for (; n < 98; n++)
+{
+_putchar(n);
+_putchar(',');
+}
+}
+else
+{
+_putchar(n);
+ }
+_putchar('\n');
 }
