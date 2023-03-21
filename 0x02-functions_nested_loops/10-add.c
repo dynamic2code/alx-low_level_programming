dcbda4624150029795afc1c1f8730cbc633a5a01
dynamic2code <dynamicdesingns@gmail.com>
2023-03-21 13:10:12 -0700

    changes to task 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- *add - a function that adds two intagers
+ *add - a function that adds two intagers.
  *
- *returns the result
+ *returns the result.
  *
  *@x : first intager
- *@y : second intager 
+ *@y : second intager
+ *Return: Add of number one and number two. 
  */
-
 int add(int x, int y)
 {
 int sum;
