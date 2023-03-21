8d89f84a65c2e4ebe6b18e394a5173d57a42f836
dynamic2code <dynamicdesingns@gmail.com>
2023-03-21 13:22:40 -0700

    changes to task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *print_to_98 - prints all natural numbers from n to 98, followed by a new line.
+ *@n :the number passed to your function
+ *RETURN :returns void
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+	for (; n > 98; n--)
+		{
+		printf("%d, ", n);
+		}
+	}
+	else if (n < 98)
+	{
+		for (; n < 98; n++)
+		{
+		printf("%d, ", n);
+		}
+	}
+	printf("%d\n", n);
+}
