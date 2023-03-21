498bfbdbc8d8512f63828ce8e292f58db83a1470
dynamic2code <dynamicdesingns@gmail.com>
2023-03-21 12:41:13 -0700

    changes to task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -13,5 +13,14 @@ int print_last_digit(int n)
 int last_number;
 
 last_number = n % 10;
+if (last_number < 0)
+{
+_putchar(-last_number + 48);
+return (-last_number);
+}
+else
+{
+_putchar(last_number + 48);
 return (last_number);
+}
 }
