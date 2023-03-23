4700403f901938eb3ff7a342b89a82884bdac99f
dynamic2code <dynamicdesingns@gmail.com>
2023-03-23 11:17:36 -0700

    addition of task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *mul -multiplies two integers
+ *@a: first integer
+ *@b: second integer
+ *Return: always 0
+ */
+
+int mul(int a, int b)
+{
+_putchar(a * b);
+return (0);
+}
