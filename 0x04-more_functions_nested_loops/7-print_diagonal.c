fa735923a7df6a018497b59336cd26c642a268f8
dynamic2code <dynamicdesingns@gmail.com>
2023-03-23 14:04:22 -0700

    task 7 script

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *print_diagonal - draws a diagonal line on the terminal
+ *@i: number of times to go to new line
+ *@j: number of blank spaces per line
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			if (i == j)
+			{
+				_putchar('\\');
+				_putchar('\n');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+		}
+	}
+}
