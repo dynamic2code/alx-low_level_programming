9686a18d802a388cba711c9c9bf12fbe5dc3c956
dynamic2code <dynamicdesingns@gmail.com>
2023-03-23 13:20:18 -0700

    addition of task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,23 +1,20 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- *more_numbers -prints 10 times the numbers, from 0 to 14
- *
- */
+* more_numbers - prints 10 times the numbers, from 0 to 14.
+*/
 void more_numbers(void)
 {
-int x;
-char y;
-for (x = 0 ; x < 10; x++)
-{
-for (y = 0; y <= 14; y++)
-{
-if (c > 9)
-{
-_putchar((c / 10) + '0');
-_putchar((c % 10) + '0');
-}
-}
-_putchar('\n');
-}
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 0; c <= 14; c++)
+		{
+			if (c > 9)
+			_putchar((c / 10) + '0');
+			_putchar((c % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
