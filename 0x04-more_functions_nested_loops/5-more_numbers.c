8cbdb5543e236854c01ec2de5358ad7bd0e1f9a0
dynamic2code <dynamicdesingns@gmail.com>
2023-03-23 13:09:24 -0700

    addition of task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *more_numbers -prints 10 times the numbers, from 0 to 14
+ *
+ */
+void more_numbers(void)
+{
+int x;
+char y;
+for (x = 0 ; x <= 9; x++)
+{
+for (y = 0; y <= 14; y++)
+{
+if (c > 9)
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+
+}
+_putchar('\n');
+}
+}
