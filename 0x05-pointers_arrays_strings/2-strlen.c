42b400ac60b692ea3aaccaf1af12fbe6d9bb526d
dynamic2code <dynamicdesingns@gmail.com>
2023-03-27 04:25:49 -0700

    task 2 script

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_strlen -  returns the length of a string.
+ *Return: returns len
+ *@s: sring to be evaluated
+ *@len: length of the string 
+ */
+int _strlen(char *s)
+{
+int len;
+len = 0;
+while (*s != '\0') {
+len++;
+s++;
+}
+}
