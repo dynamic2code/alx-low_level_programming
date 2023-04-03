d13f7dcf23b3e89ded5baf3e31ae9fbe84893102
dynamic2code <dynamicdesingns@gmail.com>
2023-04-03 13:41:44 -0700

    additional scrits to tasks

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_memcpy - copies n bytes from memory area src to memory area dest
+ *@dest: memory area
+ *@src: memory area
+ *@n: number of byts
+ *Return: a pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+char *p = dest;
+while (n-- > 0)
+{
+*p++ = *src++;
+}
+return dest;
+}
