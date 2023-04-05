5d7f1182a3b57d76ff4b00001dfe8d48cc859fc2
dynamic2code <dynamicdesingns@gmail.com>
2023-04-05 13:17:24 -0700

    additon of task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *factorial - calculates the factorial of a give number
+ *@n: the given number
+ *Returns: the factorial of a given number.
+ */
+
+int factorial(int n)
+{
+  if (n < 0)
+{
+return (-1);
+}
+else if (n == 0)
+{
+return (1)
+}
+else
+{
+return (n * factorial(n -1));
+}
+}
