2d238682daafbac8ac9a5e0ad07043cbdc0f8bc3
dynamic2code <dynamicdesingns@gmail.com>
2023-04-05 12:41:53 -0700

    addition of task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion - calculates the length of a string
+ *
+ *@s: string to be used
+ *@len: length of string
+ *Return: the length of a string
+ */
+
+int _strlen_recursion(char *s)
+{
+int len = 0;
+if (*s == '\0')
+{
+return (len);
+}
+len++;
+_strlen_recursion(s + 1);
+}
