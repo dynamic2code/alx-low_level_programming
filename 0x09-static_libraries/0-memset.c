17e00e999450ff1be563ea29dac7f82ac35bb56f
dynamic2code <dynamicdesingns@gmail.com>
2023-04-08 22:02:14 +0300

    addition of object files

---
+++ 0x09-static_libraries/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ **_memset - fill a block of memory with a specific value
+ *@s: character to fill
+ *@b: constant byte
+ *@n: size of memory
+ *Return: a pointer to the memory area s
+ *
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+char *p = s;
+while (n-- > 0)
+{
+*p++ = b;
+}
+return (s);
+}
