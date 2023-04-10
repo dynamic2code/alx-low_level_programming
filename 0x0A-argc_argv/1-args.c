090c04f14af834a059d57c659986bbbc8fe70342
dynamic2code <dynamicdesingns@gmail.com>
2023-04-10 13:35:57 -0700

    addition of task 1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- *main -  prints the number of arguments passed 
+ *nargc -  prints the number of arguments passed 
  *@argc: number of arguments
  *@argv: list of arguments
  *Return: (0)
  */
 
-int main(int argc, char *argv[])
+int nargc(int argc, char *argv[])
 {
 argc = argc;
 printf("%d\n", argc - 1);
