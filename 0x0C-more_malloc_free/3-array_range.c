f862fda6b0de915914726d7298b3db6ad06bd6aa
dynamic2code <dynamicdesingns@gmail.com>
2023-04-14 05:40:50 -0700

    additon of task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,36 @@
+
+/**
+ **array_range - creates an array of integers
+ *@min: minimum number
+ *@max: maximum number
+ *
+ *Return: pointer to the arrray on success or NULL on fail
+ */
+int *array_range(int min, int max)
+{
+int *my_array;
+int count;
+int current, i;
+
+if (min > max)
+{
+return (NULL);
+}
+count = 1;
+current = min;
+while(current >= max)
+{
+current++;
+count++;
+}
+
+my_array  = malloc(sizeof(int) * count);
+
+if(my_array == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < size && min <= max; i++, min++)
+*(p + i) = min;
+ return (my_array);
+}
