5b7c31670f2e4826d4ca004b8cf4a3642aa399e8
dynamic2code <dynamicdesingns@gmail.com>
2023-04-14 05:04:32 -0700

    changes to task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -2,22 +2,21 @@
 #include <string.h>
 /**
  **_calloc - allocates memory for an array, using malloc
- *@nmenb: number of members
+ *@nmemb: number of members
  *@size: size of elements
  *Return: pointer to array on success and Null on fail
  */
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-if(nmemb == 0 || size == 0)
+char *my_array;
+unsigned int i;
+if (nmemb == 0 || size == 0)
 {
 return (NULL);
 }
-char *my_array;
-unsigned int i;
 my_array = malloc(size * nmemb);
-
-if(my_array == NULL)
+if (my_array == NULL)
 {
 return (NULL);
 }
