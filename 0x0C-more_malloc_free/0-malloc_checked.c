76baeef235a79534dfc2ae6b15a38bd3bff40c01
dynamic2code <dynamicdesingns@gmail.com>
2023-04-14 01:10:24 -0700

    changes to task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,12 +2,21 @@
 #include <stdlib.h>
 
 /**
- *
- *
- *
+ **malloc_checked - allocates memory using malloc
+ *@d: size to be allocated
+ *Return: a pointer to the allocated memory
  */
 
 void *malloc_checked(unsigned int b)
 {
-
+int *p
+p = malloc(b);
+if (p == NULL)
+{
+exit(98);
+}
+else
+{
+return (p);
+}
 }
