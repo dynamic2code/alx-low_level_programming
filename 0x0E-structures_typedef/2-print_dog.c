8eb5efd6996dedb5357822acab640919e36e9b52
dynamic2code <dynamicdesingns@gmail.com>
2023-04-18 12:48:54 -0700

    changes to dog.h

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer to an element of type dog
+ * Return: nothing
+ **/
+
+void print_dog(struct dog *d)
+{
+  
+}
