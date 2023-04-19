347e03f24755e107cf8d04e2a1c4ad693c28e029
dynamic2code <dynamicdesingns@gmail.com>
2023-04-19 10:45:26 -0700

    first commit and addition of task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,5 @@
 #include "function_pointers.h"
 #include <stdio.h>
-#include"stdlib.h"
 /**
  * print_name - prints a name
  * @name: pointer to char
@@ -10,6 +9,5 @@
 
 void print_name(char *name, void (*f)(char *))
 {
-if (name != NULL && *name != '\0' && f != NULL)
 f(name);
 }
